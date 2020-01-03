12be886de07a55c2fb32eb18475e0ecc531415b6
SebastienDS <sebastien.dossantos.77@gmail.com>
2020-01-03 19:09:39 +0100

    CEST CA QU'IL FAUT

---
+++ test/haut.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ma page</title>
+</head>
+<body>
+	<h1> ZEGZINEGZBNEBN</h1>
---
+++ test/milieu.php
@@ -0,0 +1,5 @@
+<?php require 'haut.php'; ?>
+
+<h1> TEST TEST</h1>
+
+<?php require 'bas.php'; ?>
