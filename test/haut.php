b616d6e625106c83132a8729449f1b54c9b23b52
SebastienDS <sebastien.dossantos.77@gmail.com>
2020-01-07 23:59:24 +0100

    suppressions MERDE

--- test/haut.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Ma page</title>
-</head>
-<body>
-	<h1> ZEGZINEGZBNEBN</h1>
--- test/milieu.php
+++
@@ -1,5 +0,0 @@
-<?php require 'haut.php'; ?>
-
-<h1> TEST TEST</h1>
-
-<?php require 'bas.php'; ?>
