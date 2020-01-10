2e56801eb471b9e8767f2192c987a0688cdebd01
SebastienDS <sebastien.dossantos.77@gmail.com>
2020-01-10 14:39:08 +0100

    Update debut_base.php

--- base_php/debut_base.php
+++ base_php/debut_base.php
@@ -2,7 +2,7 @@
 if (!isset($_GET["lang"])) {
 	$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 	if ($lang != 'fr')
-	    $lang = 'eng';
+	    $lang = 'en';
 	header("Location: index.php?lang=$lang");
 }
 
