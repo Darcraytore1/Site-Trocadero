de1ac3a03c2153a2f5e63c3a0e2a14ef8902adce
Darcraytore1 <57347517+Darcraytore1@users.noreply.github.com>
2020-05-09 10:51:37 +0200

    Ajout de contenu
    Champ de Mars, palais de chaillot, jardins trocadéro

--- contenu/contenu_fr.php
+++ contenu/contenu_fr.php
@@ -4,7 +4,7 @@ define("langue", "Langue");
 
 // debut_base.php
 
-define("Titre_page", "Rallye culturel du Trocadéro");
+define("Titre_page", "Rallye culturelle du trocadéro");
 define("Accueil", "Accueil");
 define("Rallye", "Rallye");
 define("Trocadero", "Trocadero");
@@ -21,21 +21,21 @@ define("Statistiques", "Statistiques");
 define("Titre_Accueil", "Bienvenue");
 define("Contenu_Accueil", <<<contenu_accueil
 	Ce site a pour but de vous proposer un rallye culturel autour de la place du
-	Trocadéro.
+	Trocadero.
 	<br/><br/>
 	Pour ce faire sur la page rallye, il y a trois parcours en fonction du temps que
-	vous avez à consacrer.
+	vous avez à consacré.
 	<br/>
 	Un parcours court (environ une heure), un parcour moyen d'environ 3h et un parcour long d'environ 5h.
 	<br/><br/>
 	Le rallye se déroulera de la manière suivante :<br/>
-	Peu importe la longueur du parcours sélectionné, vous aurez une carte avec un point de départ se situant très proche de la place du Trocadéro. <br/>
-	Ensuite, le point sur la carte changera d'emplacement en fonction de vos déplacements afin de vous indiquer le prochain lieu à atteindre.<br/>
-	Lorsque vous etes sûr le point, vous disposerez d'informations à ce sujet ainsi qu'une question en lien avec ces informations.
+	Peu importe la longueur du parcours sélectionné vous aurez une carte avec un point de départ se situant très proche de la place du Trocadéro. <br/>
+	Ensuite, le point sur la carte changera d'emplacement en fonction de vos déplacement afin de vous indiquer le prochain lieu à atteindre.<br/>
+	Lorsque vous etes sur le point, vous disposerez d'informations à ce sujet ainsi qu'une question en lien avec ces informations. 
 	<br/><br/>
-	Ce site a été crée par un groupe d'étudiants venant de l'IUT de Marne-la-vallée de la filière DUT Informatique.
+	Ce site a été crée par un groupe d'étudiant venant de l'IUT de Marne la vallée de la filière Informatique.
 	<br/><br/>
-	Sur la page "Trocadéro", vous pouvez obtenir des informations supplémentaires sur
+	Sur la page Le Trocadero vous pouvez obtenir des informations supplémentaires sur
 	certains monuments que vous verrez pendant le rallye.
 contenu_accueil
 );
@@ -56,49 +56,70 @@ define("Champ_de_Mars", "Champ de Mars");
 // Contact.php
 
 define("Contenu_Contact", <<<contenu_contact
-	HOUCINE Thomas : Chef de Projet
+	HOUCINE Thomas : Chef
 <br/>
 	DELPORTE Logan : Design
 <br/>
-	DOS SANTOS Sebastien : Code
+	DOS SANTOS Sebastien : Codage
 <br/>
-	DELMAS Jean : Communication
+	DELMAS Jean : Communiquant
 <br/>
-	ALBISSON Damien : Code
+	ALBISSON Damien : Codage
 <br/>
-	Adresse Email: jdelmas@etud.u-pem.fr
+	Adresse Email: damienalbisson@gmail.com
 <br/>
-	Telephone : +33 6 12345678
+	Telephone : ...
 contenu_contact
 );
 
 // Jardins_du_Trocadero.php
 
 define("titre_jardins_trocadero", "Jardins du Trocadéro");
-define("Contenu_Jardins_du_Trocadero", "...");
+define("Contenu_Jardins_du_Trocadero", <<<contenu_Jardins_du_Trocadero
+Les jardins du Trocadéro sont situés dans le 16ème arrondissement, face à la Seine.
+<br>
+Ceux ci ont une superficie de 93 930 m².
+<br><br>
+Ce jardin fut créé pour l'Exposition spécialisée en 1937, 
+<br>
+il vient remplacé le précédent jardin du palais du Trocadéro (remplacé aujourd'hui par le palais de Chaillot),
+<br>
+Ce jardin est donc aujourd'hui bordé par le palais de Chaillot.
+<br><br>
+Ces jardins sont constitués d'une association de fontaines, plantations et aquarium,
+<br>
+C'est jardin sont également ponctués d'une mutitude de sculture, dont la plupart datent des années 1930.
+<br><br>
+Les allées en pente qui longent les bassins servent souvent pour faire nottament du roller
+<br>
+Apparemment, on peut y voir des "riders", descendre la pente en dérapant et en faisant des figures de fou.
+<br><br>
+Ce site est déservi par la station de métro Trocadéro.
+
+contenu_Jardins_du_Trocadero
+);
 
 // Place_du_Trocadero.php
-
 define("titre_place_trocadero", "Place du Trocadéro");
 define("Contenu_Place_du_Trocadero", <<<contenu_place_du_trocadero
 
 <br/>
-Son nom complet est en fait: La place du Trocadéro et du 11 Novembre.
+Son nom complet est enfaite: La place du Trocadéro-et-du-11-Novembre.
 Cette place se situe dans le 11ème arrondissement de Paris.
 <br/><br/>
-Son nom à été donné en souvenir de la bataille qui s'est déroulée le 31 août 1823, pendant laquelle
+Son nom à été donné en souvenir de la bataille qui s'est déroulée le 31 août 1823, pendant laquelle 
 un corps expéditionnaire français <br/>enleva aux mains des révolutionnaires libéraux espagnols le fort du Trocadéro,
-qui défendaient le port de Cadix en Espagne, et rétablit l'autorité du monarque Ferdinand VII.
+qui défendait le port de Cadix en Espagne, et rétablit l'autorité du monarque Ferdinand VII.
 <br/><br/>
-Le 11 novembre dans le nom de cette place correspond à l'armistice du 11 novembre 1918 qui
+Le 11 novembre dans le nom de cette place correspond à armistice du 11 novembre 1918 qui 
 marque la fin des combats de la Première Guerre mondiale.
 <br/><br/>
-Son diamètre est de 164 mètres, en son centre se trouve une statue équestre du maréchal Foch, et
-celle d'un monument à la gloire de l'armée française (créées toutes les deux par Paul Labdowski).
+Son diamètre est de 164 mètres, en son centre se trouve une statue équestre du maréchal Foch et 
+celle d'un monument à la gloire de l'armée française (crée toute les deux par Paul Labdowski).
 <br/><br/>
-C'est une belle place, où souffle souvent du vent : celà vient du fait qu'elle est
-très ouverte, ce vent peut embêter quelques personnes mais il nous permet cependant
-<br/> d'avoir une magnifique vue sur tout le jardin du Trocadéro, ainsi que sur la Tour Eiffel.
+C'est une belle place, ou souffle souvent du vent cela vient du fait qu'elle est 
+très ouverte, ce vent peut embêter quelques personnes mais ils nous permet cependant
+<br/> d'avoir une magnifique vue sur tout le jardin du trocadéro ainsi que bien sur la Tour Eiffel.
 
 contenu_place_du_trocadero
 );
@@ -109,33 +130,51 @@ define("titre_palais_chaillot", "Palais de Chaillot");
 define("Contenu_Palais_de_Chaillot", <<<contenu_palais_de_chaillot
 
 <br/>
-Le palais de Chaillot est situé sur la colline de Chaillot à Paris
+Le palais de Chaillot est situé sur la colline de Chaillot à Paris, dans le 16ème arrondissement de Paris.
+<br>
+Elle se situe exactement au niveau de la place du Trocadéro.
+<br><br>
+Cette édifice a réalisé lors de l'exposition universelle de 1937.
+<br>
+Concu par les architectes et grands prix de Rome Léon Azéma, Jacqques Carlu et Louis-Hippolyte Boileau.
+<br><br>
+A l'origine, à la place de ce palais de Chaillot se tenait le Palais du Trocadéro, <br> celui fut détruit
+pour être remplacé, mais le nouveau palais garda tout de même
+<br> une partie de l'ossature et la 
+configuration de celui ci, nottament ces deux ailes en demi-cercles.
+<br><br>
+Le palais de Chaillot abrite plusieurs musée en son sein :
+<br>
+- Dans son aile ouest se trouve le musée de l'Homme ainsi que celui de la marine.
+<br>
+- Dans son aile est se trouve le théâtre national de Chaillot et la cité de l'architecture et du patrimoine.
+<br><br>
+Ce site est bien évidemment déservi par la station de métro Trocadéro.
+
 
 contenu_palais_de_chaillot
 );
-
 // Tour_Eiffel.php
-
 define("titre_tour_eiffel", "Tour Eiffel");
 define("Contenu_Tour_Eiffel", <<<contenu_tour_eiffel
-
+	
 <br/>
 La Tour Eiffel est une tour de fer puddlé (fonte affiné) de 324 mètres de hauteurs (avec son antenne).
 <br/><br/>
 Elle se situe bien evidemment à Paris, dans le 7ème arrondissement, construite par Gustave Eiffel et ses
-collaborateurs pour au départ l'Exposition universelle de Paris de 1889.
+collaborateurs pour au départ l'exposition universelle de Paris de 1889. 
 
 <br/>
-Elle était au départ nommée "tour de 300 mètres", elle est aujourd'hui devenue le symbole de la capitale française.
+Elle était au départ nommée "tour de 300 mètres", elle est aujourd'hui devenue le symbole de la capital française.
 <br/><br/>
-Elle est également un gros site touristique, c'est le troisième site culturel français le plus visité, et
-le monument payant le plus visité au monde avec plus de 300 millions de visiteurs.
+Elle est également un gros site touristique, c'est le troisième site culturelle français le plus visité, et
+le monument payant le plus visité au monde avec plus de 300 millions de visiteurs
 <br/><br/>
-D'une hauteur de 312 mètres, la Tour Eiffel est resté le monument le plus élevé au monde durant quarante ans.
+D'une hauteur de 312 mètres, la Tour Eiffel est resté le monument le plus élevée au monde durant quarante ans.
 <br/><br/>
-Cette Tour pourtant connue comme le symbole de notre capitale n'est pas unique au monde :
+Cette Tour pourtant connu comme le symbole de notre capital n'est pas unique au monde 
 en effet au Japon se trouve la Tour de Tokyo.<br/>
-Celle-ci est quasiment identique à celle que nous connaissons, mais elle possède cependant une différence : 
+Celle-ci est quasiment identique à celle que nous connaissons mais elle possède cependant une différence: 
 sa couleur est rouge et blanche.
 
 contenu_tour_eiffel
@@ -144,6 +183,40 @@ contenu_tour_eiffel
 // Champ_de_Mars.php
 
 define("titre_champ_de_mars", "Champ de Mars");
-define("Contenu_Champs_de_Mars", "...");
+define("Contenu_Champs_de_Mars",<<<contenu_Champs_de_Mars
+Le Champ-de-Mars est un vaste jardin public, entièrement ouvert.<br>
+Ce jardin est situé à Paris dans le 7ème arrondissement, entre la tour Eiffel <br>
+au nord-ouest et l'Ecole militaire au sud-est. <br>
+<br>
+Elle fait 24,5 hectare, ce jardin est l'un des plus grands espaces verts de Paris.
+<br><br>
+Celui-ci accueil aujourd'hui les Parisiens et les touristes toute l'année autour d'un vaste ensemble d'activités (sauf maintenant :( )
+<br><br>
+Il accueil des concerts, des expositions ou des feux d'artifices, tous gratuits.
+<br>
+Pour citer quelques exemple de grande représentations :
+<br><br>
+- Le 10 juin 2000, Johnny Hallyday y a réalisé un concert, devant plus de 600 000 personnes.
+<br>
+- Le dimanche 13 janvier 2013, La Manif pour tous organise sur le Champ-de-Mars un rassemblement e 1 000 000 personnes
+<br>
+- Chaque année, le soir du 14 juillet, des milliers de personnes viennent assister au grand feu d'artifice de 23h.
+<br><br>
+En 1765 la construction de l'Ecole militaire, par Gabriel, entraîne en 1765 sa destination.
+<br>
+C'était donc à la base un simple grand espace de campagne.
+<br><br>
+Ce site est déservi par le métro 8, station Ecole militaire
+
+contenu_Champs_de_Mars
+);
+
+// Rallye.php
+
+define("Contenu_Rallye", <<<contenu_rallye
+
+<br/>
+contenu_rallye
+);
 
 ?>
