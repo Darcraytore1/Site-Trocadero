29cc6a47ea1ee219e595c6dd41c9313b834545f8
lgndpt <59560776+lgndpt@users.noreply.github.com>
2020-06-11 23:39:44 +0200

    Add files via upload

--- Rallye.php
+++ Rallye.php
@@ -1,14 +1,47 @@
 <?php require 'base_php/debut_base.php'; ?>
 
 <div id="main">
-	
-	<div id="map">
-		<h1>Chargement ...</h1>
-	</div>
-</div>
 
- 
-<script src="js/map.js"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyASQSLUIWa-sNKTw1PTOepRx_Ohr09Al58&callback=initMap" async defer></script>
+
+    <h2>
+        <?php echo Titre_Rallye; ?> 
+    </h2>
+    <hr class="trait-long">
+
+
+    <h3>
+        <a href="Place_du_Trocadero.php?lang=<?php echo $_GET["lang"] ?>"> <?php echo Place_du_Trocadéro; ?> </a>
+    </h3>
+    <hr class="trait-court">
+
+
+    <h3>
+        <a href="Tour_Eiffel.php?lang=<?php echo $_GET["lang"] ?>"> <?php echo Tour_Eiffel; ?> </a>
+    </h3>
+    <hr class="trait-court">
+
+
+    <h3>
+        <a href="Palais_de_Chaillot.php?lang=<?php echo $_GET["lang"] ?>"> <?php echo Palais_de_Chaillot; ?> </a>
+    </h3>
+    <hr class="trait-court">
+
+
+    <h3>
+        <a href="Jardins_du_Trocadero.php?lang=<?php echo $_GET["lang"] ?>"> <?php echo Jardins_du_Trocadéro; ?> </a>
+    </h3>
+    <hr class="trait-court">
+
+
+    <h3>
+        <a href="Champ_de_Mars.php?lang=<?php echo $_GET["lang"] ?>"> <?php echo Champ_de_Mars; ?> </a>
+    </h3>
+    <hr class="trait-court">
+
+
+    <div id="contenu">
+
+    </div>
+</div>
 
 <?php require 'base_php/fin_base.php'; ?>
